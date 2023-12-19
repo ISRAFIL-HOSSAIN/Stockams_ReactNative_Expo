bfb363c4820a3364606537a545c7186e394db26e
jihanislam69 <mdjehan301@gmail.com>
2023-12-20 02:25:45 +0600

    [pase1][UI]- Profile Tab create

--- components/global/header/BackHeader.tsx
+++ components/global/header/BackHeader.tsx
@@ -1,11 +1,15 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View,Image } from 'react-native'
 import React from 'react'
 
-const BackHeader = () => {
+const BackHeader: React.FC<any> =({ Headertext }) => {
   return (
-    <View>
-      <Text>BackHeader</Text>
-    </View>
+    <View className='flex-row items-center space-x-2 bg-white'>
+    <Image
+      className="w-[30px] h-[30px]"
+      source={require("../../../assets/images/back.png")}
+    />
+     <Text className='font-semibold text-[14px]'>{Headertext}</Text>
+  </View>
   )
 }
 
